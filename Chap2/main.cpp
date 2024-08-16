933d526214458e1845ac8d3a207b4d86a779bad1
sitRyo <al15030@shibaura-it.ac.jp>
2024-08-16 14:56:41 +0900

    feat: add 3x3 Matrix class

---
+++ Chap2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../header/Matrix3x3.hpp"
+
+int main()
+{
+    Matrix3x3 mt{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // @todo Matrix3x3 の表示
+}
--- header/Matrix3x3.hpp
+++ header/Matrix3x3.hpp
@@ -3,6 +3,9 @@
 #include "./my_concepts.hpp"
 #include "./Vector3.hpp"
 
+// @note templateでN次の正方行列を宣言できるようにすると面白そう
+// 3, 4次だけ特殊化して高速化する的な
+
 class Matrix3x3
 {
 public:
@@ -14,7 +17,7 @@ public:
     Matrix3x3 operator*(const Matrix3x3 &);
 
     // 3x1ベクトルとの乗算
-    Matrix3x3 operator*(const Vector3 &);
+    Vector3 operator*(const Vector3 &);
 
     // 単位行列への初期化
     void toIdentity();
@@ -38,6 +41,111 @@ private:
     float m31, m32, m33;
 };
 
+//=======================================================================
+Matrix3x3::Matrix3x3(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33)
+    : m11(m11), m12(m12), m13(m13), m21(m21), m22(m22), m23(m23), m31(m31), m32(m32), m33(m33)
+{
+}
+
+//=======================================================================
+Matrix3x3 Matrix3x3::operator+(const Matrix3x3 &rhs)
+{
+    m11 += rhs.m11;
+    m12 += rhs.m12;
+    m13 += rhs.m13;
+    m21 += rhs.m21;
+    m22 += rhs.m22;
+    m23 += rhs.m23;
+    m31 += rhs.m31;
+    m32 += rhs.m32;
+    m33 += rhs.m33;
+
+    return *this;
+}
+
+//=======================================================================
+Matrix3x3 Matrix3x3::operator-(const Matrix3x3 &rhs)
+{
+    m11 -= rhs.m11;
+    m12 -= rhs.m12;
+    m13 -= rhs.m13;
+    m21 -= rhs.m21;
+    m22 -= rhs.m22;
+    m23 -= rhs.m23;
+    m31 -= rhs.m31;
+    m32 -= rhs.m32;
+    m33 -= rhs.m33;
+
+    return *this;
+}
+
+//=======================================================================
+Matrix3x3 Matrix3x3::operator*(const Matrix3x3 &rhs)
+{
+    m11 = m11 * rhs.m11 + m12 * rhs.m21 + m13 * rhs.m31;
+    m12 = m11 * rhs.m12 + m12 * rhs.m22 + m13 * rhs.m32;
+    m13 = m11 * rhs.m13 + m12 * rhs.m23 + m13 * rhs.m33;
+
+    m21 = m21 * rhs.m11 + m22 * rhs.m21 + m23 * rhs.m31;
+    m22 = m21 * rhs.m12 + m22 * rhs.m22 + m23 * rhs.m32;
+    m23 = m21 * rhs.m13 + m22 * rhs.m23 + m23 * rhs.m33;
+
+    m31 = m31 * rhs.m11 + m32 * rhs.m21 + m33 * rhs.m31;
+    m32 = m31 * rhs.m12 + m32 * rhs.m22 + m33 * rhs.m32;
+    m33 = m31 * rhs.m13 + m32 * rhs.m23 + m33 * rhs.m33;
+
+    return *this;
+}
+
+//=======================================================================
+Vector3 Matrix3x3::operator*(const Vector3 &rhs)
+{
+    const auto x = m11 * rhs.X() + m12 * rhs.Y() + m13 * rhs.Z();
+    const auto y = m21 * rhs.X() + m22 * rhs.Y() + m23 * rhs.Z();
+    const auto z = m31 * rhs.X() + m32 * rhs.Y() + m33 * rhs.Z();
+
+    return Vector3{x, y, z};
+}
+
+//=======================================================================
+void Matrix3x3::toIdentity()
+{
+    m11 = 1;
+    m12 = 0;
+    m13 = 0;
+    m21 = 0;
+    m22 = 1;
+    m23 = 0;
+    m31 = 0;
+    m32 = 0;
+    m33 = 1;
+}
+
+//=======================================================================
+float Matrix3x3::det()
+{
+    return m11 * (m22 * m33 - m23 * m32) - m12 * (m21 * m33 - m23 * m31) + m13 * (m21 * m32 - m22 * m31);
+}
+
+//=======================================================================
+Matrix3x3 Matrix3x3::inv()
+{
+    const auto dt = det();
+    const float i11 = (m22 * m33 - m23 * m32) / dt;
+    const float i12 = (m13 * m32 - m12 * m33) / dt;
+    const float i13 = (m12 * m23 - m13 * m22) / dt;
+    const float i21 = (m23 * m31 - m21 * m33) / dt;
+    const float i22 = (m11 * m33 - m13 * m31) / dt;
+    const float i23 = (m13 * m21 - m11 * m23) / dt;
+    const float i31 = (m21 * m32 - m22 * m31) / dt;
+    const float i32 = (m12 * m31 - m11 * m32) / dt;
+    const float i33 = (m11 * m22 - m12 * m21) / dt;
+
+    return Matrix3x3{i11, i12, i13, i21, i22, i23, i31, i32, i33};
+}
+
+// Global Scope
+//=======================================================================
 template <IsArithmetic T>
 inline Matrix3x3 operator*(const T lhs, const Matrix3x3 &rhs)
 {
@@ -55,6 +163,7 @@ inline Matrix3x3 operator*(const T lhs, const Matrix3x3 &rhs)
     return rhs;
 }
 
+//=======================================================================
 template <IsArithmetic T>
 inline Matrix3x3 operator/(const Matrix3x3 &lhs, const T rhs)
 {
